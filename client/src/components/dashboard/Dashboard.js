259e2571829fdfd29a692101a934720019d925b0
Alpha2492 <broweralan@gmail.com>
2022-01-31 10:39:24 +0200

    Completed section 8
    All working up to section 8
    
    Signed-off-by: Alpha2492 <broweralan@gmail.com>

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// fetch all data usinmg and action
+// bring in redux state
+// push it down to other componenets (e.g experieance and education)
+
+const Dashboard = (props) => {
+  return (
+    <section className='container'>
+      <div>Dashboard</div>
+    </section>
+  );
+};
+
+Dashboard.propTypes = {};
+
+export default Dashboard;
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -24,7 +24,7 @@ router.get('/', auth, async (req, res) => {
 });
 
 // @route   POST api/auth
-// @desc    Authenticate user and get token
+// @desc    Authenticate user and get token .ie login ?
 // @access  Public
 router.post(
   '/',
